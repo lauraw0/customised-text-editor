a984ad34bb6b0ea2527b11fa010cd51a14e58145
Laura <laurawen003@gmail.com>
2022-08-07 16:05:24 +1000

    Added bold formating

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // Import React dependencies
 import React, { useCallback, useState } from 'react'
 // Import the Slate editor factory, 'Editor' and 'Transforms' helpers
-import { createEditor, Editor, Transforms } from 'slate'
+import { createEditor, Editor, Transforms, Text } from 'slate'
 import { Slate, Editable, withReact } from 'slate-react'
 
 
@@ -28,6 +28,14 @@ const DefaultElement = props => {
     )
 }
 
+const Leaf = props => {
+    return (
+        <span {...props.attributes} style={{ fontWeight: props.leaf.bold ? 'bold' : 'normal' }}>
+            {props.children}
+        </span>
+    )
+}
+
 const App = () => {
     // This Slate editor object won't change across renders as we don't set it
     const [editor] = useState(() => withReact(createEditor()))
@@ -43,12 +51,16 @@ const App = () => {
         }
     }, [])
 
+    const renderLeaf = useCallback(props => {
+        return <Leaf {...props} />
+    }, [])
 
     return (
         // Add the editable component inside the context
         <Slate editor={editor} value={initialValue} >
             <Editable
                 renderElement={renderElement}
+                renderLeaf={renderLeaf}
                 //Define a new handler which creates code blocks
                 onKeyDown={event => {
                     if (!event.ctrlKey) return
@@ -72,6 +84,14 @@ const App = () => {
                         }
                         // when "b" is pressed bold our text selection
                         case 'b': {
+                            event.preventDefault()
+                            Transforms.setNodes(
+                                editor,
+                                { bold: true },
+                                // Apply it to text nodes, splitting the nodes up
+                                // if selection is overlapping only a bit
+                                { match: n => Text.isText(n), split: true }
+                            )
                             break
                         }
                     }
